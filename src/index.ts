27d7c2b4d879d91c47617f859c22468682f331bd
Yuta4u <yudhistira4u@gmail.com>
2024-10-18 16:41:43 +0700

    b

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { Elysia } from "elysia"
 import { RouteUsers } from "./route/user.route"
 import { swagger } from "@elysiajs/swagger"
+import serverless from "serverless-http"
 
-const app = new Elysia().use(swagger()).get("/", () => "Hello Elysia")
+const app: any = new Elysia().use(swagger()).get("/", () => "Hello Elysia")
 app.use(RouteUsers)
 
 app.listen(8080)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 )
+
+// Untuk AWS Lambda
+export const handler = serverless(app)
